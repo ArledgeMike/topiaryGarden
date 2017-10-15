ddd28a73d73bdafc3b400bf63134725cb6d49a68
marledge <marledge@cars.com>
2017-10-15 15:58:01 -0500

    tighten up garden bed need to look into webpack now for JS files

--- app/server/server.js
+++ app/server/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const fs = require('fs');
-const app = express();
 const path = require('path');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../ui/views/'));
 
-app.use(express.static(__dirname + '../ui/public'));
+app.use(express.static(path.join(__dirname, '../ui/public')));
 
-app.get('/', (request, response)=>{
-    response.render('template/index');
+app.get('/', (request, response) =>  {
+    
+    const data = JSON.parse(fs.readFileSync(__dirname + '/data/data.json', 'utf8'));
+    response.render('template/index', { data });
 });
 
 app.listen(8080);
